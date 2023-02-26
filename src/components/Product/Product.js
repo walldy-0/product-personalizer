3adcdaec27a17903d5e969f14e1a21db8d81f6eb
Waldemar Szymon <walldy@tlen.pl>
2023-02-26 17:37:04 +0100

    [kodilla task 16.4] Add useMemo hook for getPrice function

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,5 +1,5 @@
 import styles from './Product.module.scss';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import ProductImage from '../ProductImage/ProductImage';
 import ProductOptions from '../ProductOptions/ProductOptions';
 
@@ -12,12 +12,14 @@ const Product = props => {
 
   const changeSize = e => setCurrentSize(e.target.getAttribute('data-size'));
 
-  const getPrice = () => props.basePrice + props.sizes.find(size => size.name === currentSize).additionalPrice;
+  const getPrice = useMemo(() => {
+    return props.basePrice + props.sizes.find(size => size.name === currentSize).additionalPrice;
+  }, [currentSize]);
 
   const addToCart = e => {
     e.preventDefault();
 
-    console.log('Summary\n==============\nName: ' + props.title + '\nPrice: ' + getPrice()
+    console.log('Summary\n==============\nName: ' + props.title + '\nPrice: ' + getPrice
       + '\nSize: ' + currentSize + '\nColor: ' + currentColor);
   };
 
@@ -27,7 +29,7 @@ const Product = props => {
       <div>
         <header>
           <h2 className={styles.name}>{props.title}</h2>
-          <span className={styles.price}>Price: {getPrice()}$</span>
+          <span className={styles.price}>Price: {getPrice}$</span>
         </header>
         <ProductOptions props={props} currentColor={currentColor} currentSize={currentSize} addToCart={addToCart} changeColor={changeColor} changeSize={changeSize} />
       </div>
